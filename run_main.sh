a77a1a80139826ba3655690f74fbd2eee1b65961
Tim Jaspers <57635970+TimJaspers0801@users.noreply.github.com>
2024-02-27 08:48:59 +0100

    Add files via upload

---
+++ run_main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=1:00:00
+
+cd /gpfs/home4/<username>/FinalAssignment
+mkdir wandb/$SLURM_JOBID
+
+srun apptainer exec --nv /gpfs/work5/0/jhstue005/JHS_data/5lsm0_v1.sif /bin/bash run_container.sh
---
+++ train.py
@@ -0,0 +1,46 @@
+"""
+This file needs to contain the main training loop. The training code should be encapsulated in a main() function to
+avoid any global variables.
+"""
+from model import Model
+from torchvision.datasets import Cityscapes
+from argparse import ArgumentParser
+
+
+def get_arg_parser():
+    parser = ArgumentParser()
+    parser.add_argument("--data_path", type=str, default=".", help="Path to the data")
+    """add more arguments here and change the default values to your needs in the run_container.sh file"""
+    return parser
+
+
+def main(args):
+    """define your model, trainingsloop optimitzer etc. here"""
+
+    # data loading
+    dataset = Cityscapes(args.data_path, split='train', mode='fine', target_type='semantic')
+
+    # visualize example images
+
+    # define model
+    model = Model().cuda()
+
+    # define optimizer and loss function (don't forget to ignore class index 255)
+
+
+    # training/validation loop
+
+
+    # save model
+
+
+    # visualize some results
+
+    pass
+
+
+if __name__ == "__main__":
+    # Get the arguments
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    main(args)
