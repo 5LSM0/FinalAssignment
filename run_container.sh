aafd2567b112619fd0c3e66ff59b5ece8ffcbcb0
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-03-07 11:48:35 +0100

    Add API key for the Weights&Biases in 'run_container.py'

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,9 @@
+export WANDB_API_KEY=cadc5cb0be3f2d907bb3a64de10fa2818f2b1a53
+export WANDB_DIR=wandb/$SLURM_JOBID
+export WANDB_CONFIG_DIR=wandb/$SLURM_JOBID
+export WANDB_CACHE_DIR=wandb/$SLURM_JOBID
+export WANDB_START_METHOD="thread"
+wandb login
+
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"
